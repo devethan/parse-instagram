e9bb9002b3398276270e1034e9ee9c309b4278d3
mydeo91 <leh2030@icloud.com>
2019-04-16 23:04:47 +0900

    Updates

---
+++ precache-manifest.a86acb59588957c4a0af967e00fa7eee.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b1a134b10558eead1417",
+    "url": "/parse-demo/static/css/main.9a17f1f5.chunk.css"
+  },
+  {
+    "revision": "b1a134b10558eead1417",
+    "url": "/parse-demo/static/js/main.9db9c76a.chunk.js"
+  },
+  {
+    "revision": "749471513a1fc8478509",
+    "url": "/parse-demo/static/js/runtime~main.40078b0c.js"
+  },
+  {
+    "revision": "dabdc0ece2ddbdefa070",
+    "url": "/parse-demo/static/js/2.452e1260.chunk.js"
+  },
+  {
+    "revision": "c3ce7e35f04c8d48235fe922bb4bb31e",
+    "url": "/parse-demo/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "mydeo91.github.io/parse-demo/precache-manifest.92e56a5e8225302c8e8b39432cc02dfe.js"
+  "/parse-demo/precache-manifest.a86acb59588957c4a0af967e00fa7eee.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("mydeo91.github.io/parse-demo/index.html", {
+workbox.routing.registerNavigationRoute("/parse-demo/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
